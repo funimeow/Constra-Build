9b327f547da3122f611abba5afc417e0bd5f54e0
funimeow <chadijawhar9@gmail.com>
2024-05-10 19:18:30 +0200

    new

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 
-    let lastScrollTop = 0;
-    window.addEventListener("scroll", function() {
-        let currentScroll = window.scrollY || document.documentElement.scrollTop;
-        
-        
-        if (currentScroll > lastScrollTop) {
-            
-            document.querySelector(".navbar").classList.add("hidden"); 
-        } else {
-            
-            document.querySelector(".navbar").classList.remove("hidden"); 
-        }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
-    }, false);
- 
